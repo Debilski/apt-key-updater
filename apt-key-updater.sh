9f077fed2811619662e9f6fb82b04a6dbcc982c7
Rike-Benjamin Schuppner <rikebs@debilski.de>
2018-07-17 11:56:56 +0200

    0.1.1

--- apt-key-updater.sh
+++ apt-key-updater.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-key adv --keyserver pgp.mit.edu --recv-keys $( apt-key finger| grep fingerprint | awk '{for(i=4;i<=NF;i++){printf "%s", $i} printf " "}' )
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $( apt-key finger| grep fingerprint | awk '{for(i=4;i<=NF;i++){printf "%s", $i} printf " "}' )
 
