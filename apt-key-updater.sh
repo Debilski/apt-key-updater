41289b498473a49323de1b9c7804ac3cce9dbd13
Rike-Benjamin Schuppner <rikebs@debilski.de>
2018-07-17 12:06:23 +0200

    More robust grep

--- apt-key-updater.sh
+++ apt-key-updater.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $( apt-key finger| grep fingerprint | awk '{for(i=4;i<=NF;i++){printf "%s", $i} printf " "}' )
+# We assume the line after pub contains the fingerprint and the fingerprint is in the last 50 characters of that line.
+# Then we remove all spaces. Finally, we have to consider differing grep implementations and filter out double newlines.
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $( apt-key fingerprint | grep -oPz '(?<=pub).*\n.*(\K.{50})\n' | sed 's: ::g' | sed  '/^$/d' )
 
